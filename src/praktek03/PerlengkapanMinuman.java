48936c99273c4b71ffaeec642d0c6da26a0937f4
EstiYuliani <32305158+EstiYuliani@users.noreply.github.com>
2018-04-09 20:34:39 +0800

    Membuat attribute dan method

--- src/praktek03/PerlengkapanMinuman.java
+++ src/praktek03/PerlengkapanMinuman.java
@@ -2,5 +2,10 @@
 package praktek03;
 
 public class PerlengkapanMinuman extends BubukMinuman{
+    private String gelasMinum;
+    private String sedotan;
     
+    void throttle(){
+        System.out.println ("GERRRRR..............");
+    }
 }
