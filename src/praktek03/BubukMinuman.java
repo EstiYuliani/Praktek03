af7917b286cb7a8d57631edccfcf2902e1bcf09b
EstiYuliani <32305158+EstiYuliani@users.noreply.github.com>
2018-04-09 19:59:36 +0800

    Membuat getter ,setter & memanggil method set

--- src/praktek03/BubukMinuman.java
+++ src/praktek03/BubukMinuman.java
@@ -14,6 +14,38 @@ public class BubukMinuman {
                            "Jenis \t: "+jenis+"\n"+
                             "Harga \t:  "+harga) ;
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getRasa() {
+        return rasa;
+    }
+
+    public void setRasa(String rasa) {
+        this.rasa = rasa;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
     
     
 }
--- src/praktek03/BubukMinumanAksi.java
+++ src/praktek03/BubukMinumanAksi.java
@@ -5,11 +5,11 @@ public class BubukMinumanAksi {
         public static void main(String[] args){
             BubukMinuman Hatta = new BubukMinuman ();
             
-            Hatta.merk="Pondok Cokelat";
-            Hatta.rasa="Choco Paradisee";
-            Hatta.jenis="Minuman Ice Blend";
-            Hatta.harga=5200;
-            
+           Hatta.setMerk("Klasik");
+           Hatta.setRasa("Green thaitea");
+           Hatta.setJenis("Minuman Hot");
+           Hatta.setHarga(8400)
+                   ;
             Hatta.cetakInfo ();
             
             
