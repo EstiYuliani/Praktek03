64e1616cc9ec2077251b1eef96020abf9b6a474b
EstiYuliani <32305158+EstiYuliani@users.noreply.github.com>
2018-04-09 19:48:42 +0800

    Instance.buat attribut dan panggil method

--- src/praktek03/BubukMinumanAksi.java
+++ src/praktek03/BubukMinumanAksi.java
@@ -3,7 +3,17 @@ package praktek03;
   
 public class BubukMinumanAksi {
         public static void main(String[] args){
+            BubukMinuman Hatta = new BubukMinuman ();
             
+            Hatta.merk="Pondok Cokelat";
+            Hatta.rasa="Choco Paradisee";
+            Hatta.jenis="Minuman Ice Blend";
+            Hatta.harga=5200;
+            
+            Hatta.cetakInfo ();
+            
+            
+                    
         }
     
 }
